024286e893004f612f84512018d0bf21d6b94b42
andersonr <anderson.rissardi@hotmail.com>
2022-05-29 19:54:38 -0300

    Continuação do curso, novos hooks

--- F1/widgets/src/components/Search.js
+++ F1/widgets/src/components/Search.js
@@ -5,6 +5,8 @@ import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [debouncedTerm, setDebouncedTerm] = useState("");
+
 
   //Não pode usar async/await direto dentro desse método, mas pode usar em uma função temporária
   useEffect(() => {
@@ -35,8 +37,39 @@ const Search = () => {
       if (term) setResults(data.query.search);
     };
 
-    search();
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: debouncedTerm,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    if (debouncedTerm) {
+      search();
+    }
+  }, [debouncedTerm]);
 
   /*
     Segundo parametro do useEffect sempre será:
@@ -50,11 +83,18 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
         <div className="content">
           <div className="header">{result.title}</div>
           {/* Como transformar string em html no react - Abre portas para XSS */}
-          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
-          
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     );
